dc9e3c96be9a512a9f6fb89dd87c704ca8e4c82e
Mehulcoder <mehul355180@gmail.com>
2020-02-07 00:45:11 +0530

    Adding Javascript to it

--- RGB Color guessing game/game.js
+++ RGB Color guessing game/game.js
@@ -1,3 +1,15 @@
-if (2+2 == 4) {
-  console.log('Hello');
+var colors = [];
+while (colors.length < 6) {
+    do {
+        var color = Math.floor((Math.random()*1000000)+1);
+    } while (colors.indexOf(color) >= 0);
+    colors.push("#" + ("000000" + color.toString(16)).slice(-6));
 }
+console.log(colors);
+
+document.getElementById("one").style.backgroundColor = colors[0];
+document.getElementById("two").style.backgroundColor = colors[1];
+document.getElementById("three").style.backgroundColor = colors[2];
+document.getElementById("four").style.backgroundColor = colors[3];
+document.getElementById("five").style.backgroundColor = colors[4];
+document.getElementById("six").style.backgroundColor = colors[5];
