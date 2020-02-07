e0380c13cd6d85a5b95fae977f3851885b10e172
Mehulcoder <mehul355180@gmail.com>
2020-02-07 19:13:40 +0530

    Added functionality

--- RGB Color guessing game/game.js
+++ RGB Color guessing game/game.js
@@ -5,20 +5,30 @@ var colors = [
   "rgb(50, 10, 100)",
   "rgb(255, 255, 255)",
   "rgb(255, 0, 255)"
-
 ];
-// while (colors.length < 6) {
-//   do {
-//     var color = Math.floor((Math.random() * 1000000) + 1);
-//   } while (colors.indexOf(color) >= 0);
-//   colors.push("#" + ("000000" + color.toString(16)).slice(-6));
-// }
+function randomColor(){
+  var a = Math.floor(Math.random()*256);
+  var b = Math.floor(Math.random()*256);
+  var c = Math.floor(Math.random()*256);
+
+  return ("rgb("+a+", "+b+", "+c+")");
+}
+for (var i = 0; i < 6; i++) {
+  colors[i] = randomColor();
+}
 console.log(colors);
-var pickedcolor = colors[3];
+
+//Adding pickedcolor randomly
+var index = Math.floor(Math.random()*(6));
+var pickedcolor = colors[index];
+// console.log(pickedcolor);
+
 var squares = document.querySelectorAll(".square");
+
 for (var i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
 }
+
 document.getElementById("one").style.backgroundColor = colors[0];
 document.getElementById("two").style.backgroundColor = colors[1];
 document.getElementById("three").style.backgroundColor = colors[2];
@@ -29,16 +39,22 @@ document.getElementById("six").style.backgroundColor = colors[5];
 
 
 for (var i = 0; i < squares.length; i++) {
-  squares[i].addEventListener("click", displayDate);
+  squares[i].addEventListener("click", func);
 
-  function displayDate() {
+  function func() {
     var clickedcolor = this.style.backgroundColor;
+    console.log(clickedcolor);
     pickedcolor = (pickedcolor);
     if(clickedcolor == pickedcolor){
-      alert("You Win!");
+      document.getElementById("try").innerHTML = "You Win";
+      document.getElementById("try").style.backgroundColor = "";
+    }else{
+      var text = document.getElementById("try");
+      text.innerHTML = "Try Again"; 
+      this.style.backgroundColor = "#232323";
+      text.style.backgroundColor = "white";
     }
-    // alert(pickedcolor);
-    // alert(clickedcolor);
+
     document.getElementById("rgb").innerHTML = clickedcolor;
   }
 
