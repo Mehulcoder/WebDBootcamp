d33c1314dfd08f7c8f30f6b5559b3284f6a89195
Mehulcoder <mehul355180@gmail.com>
2020-03-03 23:55:45 +0530

    On the way to complete the to do list project\

--- Project-ToDoList/Assets/js/todo.js
+++ Project-ToDoList/Assets/js/todo.js
@@ -1,6 +1,20 @@
-$("li").click(function (e) {
+$("ul").on("click","li" function (e) {
     $(this).toggleClass("completed");
-    
-    
-
 });
+
+
+//ul exists when the page loads so we are selecting it rather than the span
+$("ul").on("click","span" function (e) { 
+    $(this).parent().fadeOut(500, function () {
+    $(this).remove();  
+    });
+    e.stopPropagation();
+});
+
+
+$("input:text").keypress(function (e) { 
+    if (event.which === 13) {
+        $("ul").append("<li><span>X</span>"+" "+$(this).val()+"</li>");
+    }
+});
+
