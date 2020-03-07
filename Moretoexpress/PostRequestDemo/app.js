1f53fe5255d16ba8152bcf6ed455c6431b8d8b09
Mehulcoder <mehul355180@gmail.com>
2020-03-07 17:51:06 +0530

    On the way to include databases

--- Moretoexpress/PostRequestDemo/app.js
+++ Moretoexpress/PostRequestDemo/app.js
@@ -1,19 +1,27 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+// Use bodyparser(remember the code)
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {  
     res.render("home");
 });
 
-app.get("/friends", function (req, res) {
-    var friends = ["Jay", "Anushk", "Chirag", "Devaishi", "Sankalp", "Bhoomit", "Saish"]; 
-    res.render("friends", {friends:friends});
-});
+// Making it global so that others can use it too//We'll use database later to make it easier
+//As this will get lost once we restart the server
+var friends = ["Jay", "Anushk", "Chirag", "Devaishi", "Sankalp", "Bhoomit", "Saish"]; 
 
 app.post("/addfriend", function (req, res) {  
-    res.send("You have reached the post request");
+    console.log(req.body);
+    friends.push(req.body.newfriend);
+    res.redirect("/friends");
+});
+
+app.get("/friends", function (req, res) {
+    res.render("friends", {friends:friends});
 });
 
 app.listen(3000, function () {  
