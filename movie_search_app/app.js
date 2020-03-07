b1fffdb789ff5e136306b0eded9beaa98d2dc598
Mehulcoder <mehul355180@gmail.com>
2020-03-07 23:16:47 +0530

    Now just the search bar is left to be added

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,15 +1,15 @@
 var express = require("express");
 app = express();
 var request = require("request");
-
+app.set("view engine", "ejs");
 
 // Getting the results according to the query
 app.get("/results", function (req, res) { 
-    request("http://omdbapi.com/?s=harry&apikey=thewdb", function (error, response, body) { 
+    request("http://omdbapi.com/?s=dark&apikey=thewdb", function (error, response, body) { 
         if (!error && response.statusCode==200) {
 
-            var results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]);
+            var data = JSON.parse(body);
+            res.render("results", {data:data});
 
         }
      });
