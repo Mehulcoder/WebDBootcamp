ace244a190f7bf3396315db06c32cdbb3ab1d53e
Mehulcoder <mehul355180@gmail.com>
2020-03-08 00:00:52 +0530

    Added the search bar. Just the styling is left.

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -3,11 +3,19 @@ app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+//The search bar that will route to the /results page
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
 // Getting the results according to the query
 app.get("/results", function (req, res) { 
-    request("http://omdbapi.com/?s=dark&apikey=thewdb", function (error, response, body) { 
-        if (!error && response.statusCode==200) {
+    var query = req.query.search;
+    var url = "http://omdbapi.com/?s="+query+"&apikey=thewdb";
 
+    request(url, function (error, response, body) { 
+        if (!error && response.statusCode==200) {
+            // console.log(req.params);
             var data = JSON.parse(body);
             res.render("results", {data:data});
 
